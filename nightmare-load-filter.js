ced98f8345769de7bedb057f81d52fc3ea1a9cb1
Eryn Lynn <evaera@users.noreply.github.com>
2017-10-05 15:00:22 -0400

    Allow custom args to be sent to filter function

--- nightmare-load-filter.js
+++ nightmare-load-filter.js
@@ -3,23 +3,24 @@ var debug = require('debug')('nightmare:load-filter');
 module.exports = exports = function(Nightmare) {
   Nightmare.action('filter',
     function(name, options, parent, win, renderer, done) {
-      parent.on('filter', function(filter, filterFunction) {
+      parent.on('filter', function(filter, filterFunction, args) {
         win.webContents.session.webRequest.onBeforeRequest(filter, function(details, cb) {
           parent.emit('log', 'load-filter', details);
           var fn = new Function('with(this){return ' + filterFunction + '}')
             .call({});
-          fn(details, cb);
+          fn(details, cb, ...args);
         });
         parent.emit('filter');
       });
       done();
       return this;
     },
-    function(filter, filterFunction, done) {
+    function(filter, filterFunction, ...args) {
+      let done = args.pop();
       //emit the filter to the child process
       debug('issuing filter: ' + require('util')
         .inspect(filter));
       this.child.once('filter', done);
-      this.child.emit('filter', filter, String(filterFunction));
+      this.child.emit('filter', filter, String(filterFunction), args);
     });
 };
